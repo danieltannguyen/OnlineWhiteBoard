094d11d700843c94d6ce0ca5de5be7913041f392
Daniel <danieltn91@gmail.com>
2017-04-13 01:25:40 -0700

    fixed database link

--- config/database.js
+++ config/database.js
@@ -3,6 +3,6 @@ module.exports = {
 
      // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
 
-    'url': 'mongodb://<heroku_sf2x42cj>:<pass123>@ds127300.mlab.com:27300/heroku_sf2x42cj'
+    'url': 'mongodb://<owner>:<pass123>@ds127300.mlab.com:27300/heroku_sf2x42cj'
 
 };

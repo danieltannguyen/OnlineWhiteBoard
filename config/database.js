327e1d59a8e8abd60c52157f77036c8dd28d3612
Daniel <danieltn91@gmail.com>
2017-04-13 00:03:16 -0700

    added Local registration using Nodejs,MongoDB, passport

---
+++ config/database.js
@@ -0,0 +1,8 @@
+// config/database.js
+module.exports = {
+
+    'url' : 'mongodb://localhost:27017/onlinewhiteboard' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+
+
+
+};
--- server.js
+++ server.js
@@ -2,7 +2,41 @@ var express =require('express');
 var app = express();
 
 //set port
-var port = process.env.PORT || 4000;
+var port = process.env.PORT || 27017;
+
+var mongoose = require('mongoose');
+var passport = require('passport');
+var flash    = require('connect-flash');
+
+var morgan       = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser   = require('body-parser');
+var session      = require('express-session');
+
+var configDB = require('./config/database.js');
+
+// configuration ===============================================================
+mongoose.connect(configDB.url); // connect to our database
+
+require('./config/passport')(passport); // pass passport for configuration
+
+// set up our express application
+app.use(morgan('dev')); // log every request to the console
+app.use(cookieParser()); // read cookies (needed for auth)
+app.use(bodyParser()); // get information from html forms
+
+app.set('view engine', 'ejs'); // set up ejs for templating
+
+// required for passport
+app.use(session({ secret: 'iloveonlinewhiteboard' })); // session secret
+app.use(passport.initialize());
+app.use(passport.session()); // persistent login sessions
+app.use(flash()); // use connect-flash for flash messages stored in session
+
+// routes ======================================================================
+require('./app/routes.js')(app, passport); // load our routes and pass in our app and fully configured passport
+
+
 (function() {
   var io;
   io = require('socket.io').listen(4000);
@@ -24,5 +58,5 @@ app.get("/", function (req,res) {
 });
 
 app.listen(port, function() {
-  console.log("app is running on port 4000!")
+  console.log("app is running on port 27017!")
 })

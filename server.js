ff93a2f2943c6ae04a2416350df8a84e11bcd949
Daniel Nguyen <danieltn91@gmail.com>
2017-08-07 14:45:13 -0700

    fix internal error 504

--- server.js
+++ server.js
@@ -4,10 +4,10 @@ var app = express();
 //set port
 var port = process.env.PORT || 5000;
 
-//app.use(express.static(__dirname));
+app.use(express.static(__dirname));
 
 app.get("/", function (req,res) {
-  res.render("whiteboard.html");
+  res.render("index.html");
 });
 
 app.listen(port, function() {
